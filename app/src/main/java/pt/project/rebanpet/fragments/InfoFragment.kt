05c84a16189cff0717a92e4811440e6fb79ae565
tiss05 <tiago05santos05@gmail.com>
2025-03-02 22:57:18 +0000

    App Customization

--- app/src/main/java/pt/project/rebanpet/fragments/InfoFragment.kt
+++ app/src/main/java/pt/project/rebanpet/fragments/InfoFragment.kt
@@ -15,7 +15,6 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_info, container, false)
         return ComposeView(requireContext()).apply {
             setContent {
                 MapKennels()
